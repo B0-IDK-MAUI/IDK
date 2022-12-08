0e624fb2482741353f594f0b4a7661961543f4ce
Botaa <84439095+Botaa@users.noreply.github.com>
2022-12-08 09:09:51 +0100

    GCcollectupdate

---
+++ Botond/GcCollector/GcCollector/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GcCollector
+{
+    internal class Program
+    {
+        public class MyClass
+        {
+            public static int Count = 0;
+            public MyClass()
+            {
+                Count++;
+                Console.WriteLine(Count);
+            }
+             ~MyClass()
+            {
+                Count--;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                MyClass myObj = new MyClass();
+                
+            }
+        }
+    }
+}
